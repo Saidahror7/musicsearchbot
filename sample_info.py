fcf2fec2368878eaff99e13b7b228ca6b15f054f
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-03-25 09:22:18 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -22,7 +22,7 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**Hi, I'm Anime Music Serach bot**
 
 Here you can search Anime Musics in inline mode. Just press following buttons and start searching.
 
